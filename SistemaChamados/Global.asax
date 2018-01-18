9fd92a242208927c5b204a8a720a85f0ca81a49d
Daniel Haro <contato.danielharo@gmail.com>
2018-01-18 18:53:08 -0200

    Adicionar arquivos de projeto.

---
+++ SistemaChamados/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaChamados.MvcApplication" Language="C#" %>
---
+++ SistemaChamados/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaChamados.Startup))]
+namespace SistemaChamados
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
